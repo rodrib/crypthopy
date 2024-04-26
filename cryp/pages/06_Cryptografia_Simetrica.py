2d646dd43cf2d744df3dff92963b1098d4cfbe0e
rodrib <rodribogado50@gmail.com>
2024-04-25 23:40:56 -0300

    create criptografia simetrica

---
+++ cryp/pages/06_Cryptografia_Simetrica.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Criptografía Simétrica")
+
+st.write("Un 'cypher' o 'cipher' es una técnica o algoritmo utilizado para cifrar o codificar información de manera que sea incomprensible para aquellos que no tengan la clave o el conocimiento necesario para descifrarlo.")
