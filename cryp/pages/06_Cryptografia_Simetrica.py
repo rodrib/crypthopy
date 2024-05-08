8624a7f01c1440e5af5af09fcc6de6e32526bc6e
rodrib <rodribogado50@gmail.com>
2024-05-07 23:52:31 -0300

    update criptografia simetrica

--- cryp/pages/06_Cryptografia_Simetrica.py
+++ cryp/pages/06_Cryptografia_Simetrica.py
@@ -18,26 +18,7 @@ st.write("""Las 'rondas de encriptación' son pasos repetitivos que se aplican e
          El objetivo de las rondas de encriptación es incrementar la resistencia del cifrado ante métodos criptoanalíticos, haciendo que sea más difícil para un atacante descifrar el mensaje sin conocer la clave correcta.""")
 
 
-from streamlit.components.v1 import html as html_component
 
-latex_code = r"""
-\documentclass[tikz,border=2mm]{standalone}
-\begin{document}
-\begin{tikzpicture}[scale=1.5]
-% Rondas de encriptación
-\foreach \x in {0,1,2,3}{
-    \draw (\x*1.5,0) rectangle ++(1,1) node[midway] {$R_{\x}$};
-}
-% Conexiones
-\foreach \x in {0,1,2}{
-    \draw[->] (\x*1.5+1,0.5) -- (\x*1.5+1.5,0.5);
-}
-\draw[->] (3*1.5+1,0.5) -- (3*1.5+1.5,0.5) node[right] {Salida};
-\draw[dotted] (4.5,0) -- (4.5,1) node[midway, above] {Proceso de cifrado};
-\draw[dotted] (0,-0.5) -- (4.5,-0.5) node[midway, below] {Rondas};
-\end{tikzpicture}
-\end{document}
-"""
 
 st.subheader("Diagrama de rondas de encriptación")
 
