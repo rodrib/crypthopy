b778b6c2f66896421c848060a0b7b5e82d4a0b86
rodrib <rodribogado50@gmail.com>
2024-06-05 22:44:08 -0300

    update ca

--- cryp/pages/07_🔒🔑🗝️Criptografia_Asimetrica.py
+++ cryp/pages/07_🔒🔑🗝️Criptografia_Asimetrica.py
@@ -137,3 +137,76 @@ animation_url_4 = "https://mantimantilla.github.io/Theory-of-Computation-Encrypt
 
 # Mostrar la cuarta animación desde la URL
 st.markdown(f'<img src="{animation_url_4}" alt="Proceso de mezclar valores en el intercambio de claves Diffie-Hellman">', unsafe_allow_html=True)
+
+
+# Quinta parte de la descripción
+st.markdown("""
+Una vez generados estos cifrados, Alice y Bob los intercambian. Note que como aún no hay un canal seguro de comunicación, `ag` y `bg` son públicos.
+""")
+
+# URL de la quinta animación GIF
+animation_url_5 = "https://mantimantilla.github.io/Theory-of-Computation-Encryption/_images/Animacion-5.gif"
+
+# Mostrar la quinta animación desde la URL
+st.markdown(f'<img src="{animation_url_5}" alt="Intercambio de cifrados en el intercambio de claves Diffie-Hellman">', unsafe_allow_html=True)
+
+# Sexta parte de la descripción
+st.markdown("""
+Nuevamente realizamos la operación de ‘mezclado’. Alice mezclará el cifrado público que acaba de recibir de Bob con su clave privada (`bg` con `a`, produce `abg`). Bob, hará lo mismo con su clave privada y el cifrado público que recibió de Alice (`ag` con `b`, produce `abg`).
+""")
+
+# URL de la sexta animación GIF
+animation_url_6 = "https://mantimantilla.github.io/Theory-of-Computation-Encryption/_images/Animacion-6.gif"
+
+# Mostrar la sexta animación desde la URL
+st.markdown(f'<img src="{animation_url_6}" alt="Operación de mezclado en el intercambio de claves Diffie-Hellman">', unsafe_allow_html=True)
+
+
+# Séptima parte de la descripción
+st.markdown("""
+Note que ahora Alice y Bob comparten un cifrado irreversible (idéntico para ambos) el cual pueden utilizar para encriptar cualquier mensaje consecuente. Alice y Bob pueden ahora establecer un canal de comunicación seguro, con encriptación simétrica.
+
+Veamos ahora, que únicamente Alice y Bob pueden generar la llave simétrica `abg`. El público tiene acceso al generador `g`, el valor `p` y las llaves cifradas `ag` y `bg`. No hay forma de ‘mezclar’ estos valores de forma tal que generen `abg`. Intentar ‘mezclar’ `bg` con `ag` produciría algo como `abgg`, lo cual es un valor completamente distinto y que no sirve para descifrar los mensajes entre Alice y Bob. ¡Hemos establecido un canal seguro de encriptación simétrica!
+""")
+
+# URL de la séptima animación GIF
+animation_url_7 = "https://mantimantilla.github.io/Theory-of-Computation-Encryption/_images/Animacion-7.gif"
+
+# Mostrar la séptima animación desde la URL
+st.markdown(f'<img src="{animation_url_7}" alt="Establecimiento de canal seguro en el intercambio de claves Diffie-Hellman">', unsafe_allow_html=True)
+
+
+import streamlit as st
+
+st.markdown("""
+Vulnerabilidades
+---
+
+El procedimiento parece robusto, pero su intención es solo escudar a 2 partidos de un ojo fisgón. ¿Qué pasa cuando introducimos a una tercera parte, Mallory, que tiene el poder de interceptar y manipular mensajes entre partes en la red? Tenemos también la preocupación de que información secreta de alguna de las partes sea liberada al público. ¿Podemos satisfacer que los mensajes previos están seguros?
+
+Ataque de intermediario (Man in the Middle)
+---
+
+Suponga un mismo escenario donde Alice y Bob quieren establecer un canal de comunicación de encriptación simétrica, solo que ahora una tercera parte, Mallory, puede intervenir sobre la información que viaja en la red. Por ejemplo, si Alice quisiera mandar su propuesta de un generador `g` a Bob, Mallory sabría que esto está sucediendo evitar que esto suceda, o escoger su propio mensaje para enviarle a Bob. Note que hasta ahora, nuestro método de comunicación no tiene forma de validar el origen de un mensaje en la red, por lo que Mallory puede estar seguro de que su intromisión en el canal de Alice y Bob no es detectable (aún).
+
+El concepto de este ataque es bastante sencillo. Todo lo que debe hacer Mallory para poder interpretar todos los futuros mensaje entre Alice y Bob es suplantarlos al momento de generar la llave compartida, de forma que Mallory comparte una llave con Bob y una con Alice.
+
+1. Alice y Bob acuerdan un `g` y `p` públicos y generan sus llaves privadas respectivas.
+2. Alice genera `ag` y lo publica en la red con la intención de que lo reciba Bob. Bob hace lo mismo para Alice con `bg`.
+3. Mallory intercepta estos mensajes y evita que lleguen al destinatario correspondiente.
+4. Mallory genera su propia llave privada `m` y produce `mg`.
+5. Mallory responde a Alice con `mg` suplantando a Bob. Alice cree que lo que acaba de recibir es `bg`.
+6. Mallory responde a Bob con `mg` suplantando a Alice. Bob cree que lo que acaba de recibir es `ag`.
+7. Alice y Bob calculan respectivamente `mag` y `mbg`.
+8. Mallory calcula `mag` y `mbg`. Ahora comparte un secreto con el cual establecer un canal encriptado simétrico con ambos Alice y Bob. Estos no se han percatado del ataque.
+
+Cualquier cifrado enviado por Alice con la intención de que lo reciba únicamente Bob, llega primero a Mallory. Mallory puede decidir hacer lo que quiera con este. Si la intención es solo mediar la comunicación entre Alice y Bob, Mallory debe descifrar el cifrado entrante y encriptarlo con el secreto contrario. Esto le permite editarlo, borrarlo, o incluso compartirlo con otros.
+
+Esta vulnerabilidad fue descubierta pronto, por lo cual se volvió una práctica común agregar un paso de autenticación al cifrado simétrico. Entran ahora en juego las llaves públicas.
+
+Forward secrecy (PFS)
+---
+
+Forward secrecy es la intención de proteger mensajes pasados en caso de que su cifrado haya sido guardado por terceros y el secreto compartido sea publicado. En este caso, la responsabilidad sobre la liberación de estos mensajes no recae sobre alguna falla de Diffie-Hellman. En un inicio mencionamos la noción de una sesión de comunicación. La idea de tener sesiones cortas sobre las cuales habilitar el canal de comunicación es beneficiosa ya que cada vez que se inicie una sesión, la llave compartida puede generarse desde cero en cada ocasión, de forma que, si se libera la clave, solo los mensajes de esa sesión son vulnerables. En teoría, nada impide que utilicemos una nueva llave para cada mensaje o para cada sub-cadena de 126 bits o menos. Recordemos que comunicarse por encriptación asimétrica es un proceso costoso, por lo que quisiéramos minimizar su uso. Cada vez que intercambiamos claves y generamos un secreto compartido estamos efectuando encriptación asimétrica, ya depende de los dueños del canal establecer la frecuencia con la que se generan estas llaves efímeras.
+""")
+
