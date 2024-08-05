0296502e2d50bc47a94baef35a3997d33bfc4d3f
rodrib <rodribogado50@gmail.com>
2024-08-05 16:43:03 -0300

    update cee y yo

--- cryp/pages/10_🎓🧐🍩Yo y las ECC.py
+++ cryp/pages/10_🎓🧐🍩Yo y las ECC.py
@@ -3,6 +3,15 @@ import streamlit as st
 # Título en Streamlit
 st.title("Yo y las ECC")
 
+resumen = """
+Mi historia con respecto a las curvas elipticas viene a traves del Teorema de Fermat, el cual conoci
+a la edad de 11  años con la Historia de Andrew Wiles, aunque entendi lo basico quede tambien bastante curioso
+con las curvas elipticas, las cuales recien volvi a su estudio ahora que empeze con la criptografia
+"""
+
+
+st.write(resumen)
+
 # Enlace a un documento relacionado
 link = "https://web.archive.org/web/20170811071550id_/http://scienzamedia.uniroma2.it/~eal/Wiles-Fermat.pdf"
 st.markdown(f"Enlace al documento relacionado: [{link}]({link})")
