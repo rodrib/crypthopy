4d5317838b05d9e26859d2d93a0564893ab245ef
rodrib <rodribogado50@gmail.com>
2024-08-15 20:16:02 -0300

    update app

--- cryp/pages/11_Ransomware.py
+++ cryp/pages/11_Ransomware.py
@@ -38,33 +38,32 @@ En resumen, el ransomware criptográfico emplea el cifrado simétrico para bloqu
 
 st.write(texto2)
 
-# Funciones
-def generarKey():
-    key = Fernet.generate_key()
-    with open("key.key", "wb") as key_file:
-        key_file.write(key)
-    return key
+# # Funciones
+# def generarKey():
+#     key = Fernet.generate_key()
+#     with open("key.key", "wb") as key_file:
+#         key_file.write(key)
+#     return key
 
-def encryp(file, key):
-    fernet = Fernet(key)
-    file_data = file.read()
-    encrypted_data = fernet.encrypt(file_data)
-    return file_data, encrypted_data
+# def encryp(file, key):
+#     fernet = Fernet(key)
+#     file_data = file.read()
+#     encrypted_data = fernet.encrypt(file_data)
+#     return file_data, encrypted_data
 
-def mostrar_contenido_pdf(file):
-    pdf_reader = PdfReader(file)
-    texto_pdf = ""
-    for page in pdf_reader.pages:
-        texto_pdf += page.extract_text()
-    return texto_pdf
+# def mostrar_contenido_pdf(file):
+#     pdf_reader = PdfReader(file)
+#     texto_pdf = ""
+#     for page in pdf_reader.pages:
+#         texto_pdf += page.extract_text()
+#     return texto_pdf
 
-# Generar y mostrar la clave
-st.title("Cifrador de Archivos PDF")
+# # Generar y mostrar la clave
+# st.title("Cifrador de Archivos PDF")
 
-
-key = generarKey()
-st.write("Clave generada:")
-st.code(key.decode(), language="text")
-
-uploaded_file = st.file_uploader("Cargar un archivo PDF", type="pdf")
-
+# """ 
+# key = generarKey()
+# st.write("Clave generada:")
+# st.code(key.decode(), language="text") 
+#  
+# uploaded_file = st.file_uploader("Cargar un archivo PDF", type="pdf") 
